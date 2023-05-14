24c982a6ce79da41540c11e8f579b7527fc06918
Ian Barreto <ian.barreto.700@ufrn.edu.br>
2023-05-14 11:57:59 -0300

    Model e DAO de Conta

---
+++ SistemaBancario/src/br/ufrn/sistemabancario/dao/ContaDAO.java
@@ -0,0 +1,52 @@
+package br.ufrn.sistemabancario.dao;
+
+import java.util.HashMap;
+
+import br.ufrn.sistemabancario.model.Conta;
+
+public class ContaDAO {
+
+	private HashMap<Long, Conta> contas;
+
+	public ContaDAO() {
+		super();
+		
+		this.contas = new HashMap<Long, Conta>();
+	}
+	
+	public void cadastrarConta(long numeroDaConta) {
+		
+		
+		Conta novaConta = new Conta(numeroDaConta);
+		contas.put(numeroDaConta, novaConta);
+		
+	}
+	
+	public double consultarSaldo(long numeroDaConta) {
+		Conta c = contas.get(numeroDaConta);
+		
+		return c.getSaldo();
+	}
+	
+	public void credito(long numeroDaConta, double valor) {
+		
+		Conta c = contas.get(numeroDaConta);
+		
+		double novoSaldo = c.getSaldo() + valor;
+		c.setSaldo(novoSaldo);
+	}
+	
+	public void debito(long numeroDaConta, double valor) {
+		
+		Conta c = contas.get(numeroDaConta);
+		
+		double novoSaldo = c.getSaldo() - valor;
+		c.setSaldo(novoSaldo);
+	}
+	
+	public void transferencia(long origem, long destino, double valor) {
+		//TODO
+	}
+	
+	
+}
